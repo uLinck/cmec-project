ec0b74b8b0203e7cc70c344833a9ab0ae0710abb
uFleck <bruno.koster@proton.me>
2024-06-24 20:23:34 -0300

    ✨[CMEC-19] - phosphor-icons installed successfully

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Acorn } from '@phosphor-icons/react'
 
 function App() {
     return (
         <div className="App">
-            Projetinho
+            <Acorn size={32} />
         </div>
     );
 }
