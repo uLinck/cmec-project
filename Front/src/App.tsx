f745ca50ba84a86b10a5f1ae4bcc7a57626ab140
RafaelSantini23 <rafaelfarias.santini14@gmail.com>
2024-06-24 21:23:55 -0300

    FEAT: ADICIONANDO MATERIAL UI

--- Front/src/App.tsx
+++ Front/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
     <div className="App">
-      Projetinho
+      <Button variant="contained">BUTAO</Button>
     </div>
   );
 }
