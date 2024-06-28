4c65a6ffa0284f9b6195aefe351aa5b28e934f20
uFleck <bruno.koster@proton.me>
2024-06-27 21:19:50 -0300

    🚧[CMEC-6] - Almost finishing search component, lacking some color

--- Front/src/Components/Search/index.tsx
+++ Front/src/Components/Search/index.tsx
@@ -1,7 +1,18 @@
+import { Button } from "@mui/material";
+import { MagnifyingGlass } from "phosphor-react";
+
 export default function Search() {
     return (
-        <div className="bg-stone-800 w-max p-2 rounded-md text-white">
+        <Button
+            variant="contained"
+            startIcon={<MagnifyingGlass size={16} />}
+            sx={{
+                textTransform: "none",
+                padding: "0.25rem 1.5rem",
+                width: "25vw",
+                fontWeight: "normal"
+            }}>
             Pesquisar...
-        </div>
+        </Button>
     );
 }
