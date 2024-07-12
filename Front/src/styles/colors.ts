e0e2e8f6f828faff1eb87219a074deefbd467ba3
uFleck <bruno.koster@proton.me>
2024-07-11 23:16:52 -0300

    🐛[CMEC-20] - Added missing '#' to colors

--- Front/src/styles/colors.ts
+++ Front/src/styles/colors.ts
@@ -1,18 +1,18 @@
 let colors = {
     primary100: '#003688',
-    primary80: '4075C5',
-    primary60: '7498CF',
+    primary80: '#4075C5',
+    primary60: '#7498CF',
 
     accent100: '#CED85A',
     accent80: '#E4ED80',
 
-    neutral100: '000813',
-    neutral80: '0F1928',
-    neutral60: '324052',
-    neutral40: '627389',
-    neutral20: '93A0B0',
-    neutral10: 'BCC8D8',
-    neutral0: 'F9FBFF'
+    neutral100: '#000813',
+    neutral80: '#0F1928',
+    neutral60: '#324052',
+    neutral40: '#627389',
+    neutral20: '#93A0B0',
+    neutral10: '#BCC8D8',
+    neutral0: '#F9FBFF'
 }
 
 export default colors
