9b9e3d37b22721a82dcaea97aeae376bf68502d8
RafaelSantini23 <rafaelfarias.santini14@gmail.com>
2024-07-13 00:00:18 -0300

    💄[CMEC-21] - Adding react-router-dom route management lib

--- Front/src/index.tsx
+++ Front/src/index.tsx
@@ -5,7 +5,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import App from './App';
+import Router from './router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>
 );
 
