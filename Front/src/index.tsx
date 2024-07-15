acba478592d9a0cef2d95f352b6467d41663beaf
uFleck <bruno.koster@proton.me>
2024-07-15 20:42:43 -0300

    🚑️[CMEC-6] - Fixed some merge errors

--- Front/src/index.tsx
+++ Front/src/index.tsx
@@ -7,7 +7,7 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import Router from './router';
 import { ThemeProvider } from '@mui/material';
-import theme from './styles/material-ui';
+import theme from './Styles/material-ui';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,7 +16,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-        <Router />
+      <Router />
     </ThemeProvider>
   </React.StrictMode>
 );
