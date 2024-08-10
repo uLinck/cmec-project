3c67bf032c5c711840ab3f325029dd6380160605
uFleck <bruno.koster@proton.me>
2024-08-09 22:37:43 -0300

    ✨[CMEC-10] - Finished PedagogicalActivities section

--- Front/src/Pages/Home/index.tsx
+++ Front/src/Pages/Home/index.tsx
@@ -1,17 +1,27 @@
 import { Box } from "@mui/material";
 import { Infraestructure } from "../../Components/Sections/Infraestructure";
+import { PedagogicalActivities } from "../../Components/Sections/PedagogicalActivities";
+import colors from "../../Styles/colors";
 
 const Home = () => {
     return (
         <Box
             sx={{
-                maxWidth: "100vw"
+                maxWidth: "100vw",
             }}
         >
             <Box sx={{ height: "100vh" }} />
-            <Box sx={{ height: "100vh" }} />
-            <Infraestructure />
-            <Box sx={{ height: "100vh" }} />
+
+            <Box
+                sx={{
+                    background: `linear-gradient(20deg, ${colors.neutral80} 0%, ${colors.neutral60} 50%, ${colors.primary60} 100%)`
+                }}
+            >
+                <Infraestructure />
+                <Box sx={{ height: "30vh" }} />
+                <PedagogicalActivities />
+                <Box sx={{ height: "100vh" }} />
+            </Box>
         </Box>
     )
 }
