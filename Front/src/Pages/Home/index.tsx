2fbddf84a412eede9b6cb22b2e9d54e8aa4525ac
uFleck <bruno.koster@proton.me>
2024-09-15 20:24:01 -0300

    ✨[CMEC-11] - Adjusted some details for integration with main's home page

--- Front/src/Pages/Home/index.tsx
+++ Front/src/Pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import { Box } from "@mui/material";
 import { Infraestructure } from "../../Components/Sections/Infraestructure";
+import { PedagogicalActivities } from "../../Components/Sections/PedagogicalActivities";
+import colors from "../../Styles/colors";
+import { Header } from "../../Components/Header";
 import { UsefulServices } from "../../Components/Sections/UsefulServices";
 
 const Home = () => {
@@ -10,9 +13,17 @@ const Home = () => {
             }}
         >
             <Box sx={{ height: "100vh" }} />
-            <Box sx={{ height: "100vh" }} />
-            <Infraestructure />
-            <UsefulServices />
+            <Box
+                id="body"
+                sx={{
+                    background: `linear-gradient(20deg, ${colors.neutral80} 0%, ${colors.neutral60} 50%, ${colors.primary60} 100%)`
+                }}
+            >
+                <Infraestructure />
+                <Box sx={{ height: "30vh" }} />
+                <PedagogicalActivities />
+                <UsefulServices />
+            </Box>
         </Box>
     )
 }
