d112165a36dae7399289bd3c2bdcdc5e42720afc
uFelker <felkeramanda690@gmail.com>
2024-10-14 15:14:53 -0300

    ✨ [CMEC-8] - implementing presentation part

--- Front/src/Pages/Home/index.tsx
+++ Front/src/Pages/Home/index.tsx
@@ -3,29 +3,31 @@ import { Infraestructure } from "../../Components/Sections/Infraestructure";
 import { PedagogicalActivities } from "../../Components/Sections/PedagogicalActivities";
 import colors from "../../Styles/colors";
 import { Header } from "../../Components/Header";
+import { AboutUs } from "../../Components/Sections/AboutUs";
 
 const Home = () => {
-    return (
-        <Box
-            sx={{
-                maxWidth: "100vw",
-            }}
-        >
-            <Header bodyQuerySelector="#body" />
-            <Box sx={{ height: "100vh" }} />
-            <Box
-                id="body"
-                sx={{
-                    background: `linear-gradient(20deg, ${colors.neutral80} 0%, ${colors.neutral60} 50%, ${colors.primary60} 100%)`
-                }}
-            >
-                <Infraestructure />
-                <Box sx={{ height: "30vh" }} />
-                <PedagogicalActivities />
-                <Box sx={{ height: "100vh" }} />
-            </Box>
-        </Box>
-    )
-}
+  return (
+    <Box
+      sx={{
+        maxWidth: "100vw",
+      }}
+    >
+      <Header bodyQuerySelector="#body" />
+      <Box sx={{ height: "100vh" }} />
+      <Box
+        id="body"
+        sx={{
+          background: `linear-gradient(20deg, ${colors.neutral80} 0%, ${colors.neutral60} 50%, ${colors.primary60} 100%)`,
+        }}
+      >
+        <AboutUs />
+        <Infraestructure />
+        <Box sx={{ height: "30vh" }} />
+        <PedagogicalActivities />
+        <Box sx={{ height: "100vh" }} />
+      </Box>
+    </Box>
+  );
+};
 
 export default Home;
