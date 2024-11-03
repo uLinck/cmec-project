ec46cff99b19805061f64cb06df761db9953ef01
uFleck <bruno.koster@proton.me>
2024-11-03 18:42:35 -0300

    🔥[CMEC-22] - Removed example files

--- Front/src/router.tsx
+++ Front/src/router.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Home from './Pages/Home';
-import ExamplePage from './Pages/Example';
 
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/example" element={<ExamplePage />} />
       </Routes>
     </BrowserRouter>
   );
