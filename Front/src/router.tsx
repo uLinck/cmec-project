7ca60509445263f6df36b5514b2025931d965e4d
uFleck <bruno.koster@proton.me>
2024-11-03 18:43:24 -0300

    ✨[CMEC-22] - Finished DocTemplate

--- Front/src/router.tsx
+++ Front/src/router.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Home from './Pages/Home';
+import Doc from './Pages/Templates/DocTemplate';
+import { Box } from '@mui/material';
 
 
 function App() {
@@ -9,6 +11,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/teste" element={
+          <Box sx={{ height: '100vh', width: '80vw', marginLeft: '20vw', maxWidth: '100vw' }}>
+            <Doc category='Testes' title='Teste 1' description='Teste realizado por macacos'>
+              <Box>Cuzinho!!!!</Box>
+            </Doc>
+          </Box>} />
       </Routes>
     </BrowserRouter>
   );
